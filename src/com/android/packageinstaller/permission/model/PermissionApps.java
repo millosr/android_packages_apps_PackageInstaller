4c96dd2cbd3245ca84396c12e263944d90e73d04
mickybart <mickybart@pygoscelis.org>
2016-09-12 06:02:17 -0400

    Superuser: Manage AppOps for OP_SU
    Create an unofficial group Superuser.
    This group is created inside PackageInstaller and it is not
    visible outside.
    
    There is no specific permission for an application to support Superuser
    in AndroidManifest so we intercept everything relative to Superuser group
    to grant/revoke permissions with AppOpsManager.
    
    Multiuser is supported

--- src/com/android/packageinstaller/permission/model/PermissionApps.java
+++ src/com/android/packageinstaller/permission/model/PermissionApps.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2015 The Android Open Source Project
+ * Copyright (C) 2016 nAOSProm
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,6 +16,7 @@
  */
 package com.android.packageinstaller.permission.model;
 
+import android.app.AppOpsManager;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
@@ -34,6 +36,7 @@ import android.util.SparseArray;
 
 import com.android.packageinstaller.R;
 import com.android.packageinstaller.permission.utils.Utils;
+import com.android.packageinstaller.R;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -44,6 +47,7 @@ public class PermissionApps {
 
     private final Context mContext;
     private final String mGroupName;
+    private final AppOpsManager mAppOps;
     private final PackageManager mPm;
     private final Callback mCallback;
 
@@ -65,6 +69,7 @@ public class PermissionApps {
     public PermissionApps(Context context, String groupName, Callback callback, PmCache cache) {
         mCache = cache;
         mContext = context;
+        mAppOps = (AppOpsManager)context.getSystemService(Context.APP_OPS_SERVICE);
         mPm = mContext.getPackageManager();
         mGroupName = groupName;
         mCallback = callback;
@@ -137,6 +142,12 @@ public class PermissionApps {
     }
 
     private List<PermissionApp> loadPermissionApps() {
+        /* Unsupported groups */
+        if (Utils.SU_GROUP.equals(mGroupName)) {
+            return loadPermissionAppOps(AppOpsManager.OP_SU);
+        }
+
+        /* Normal groups */
         PackageItemInfo groupInfo = getGroupInfo(mGroupName);
         if (groupInfo == null) {
             return Collections.emptyList();
@@ -220,6 +231,64 @@ public class PermissionApps {
 
         return permApps;
     }
+    
+    private List<PermissionApp> loadPermissionAppOps(int code) {
+        PackageInfo appInfo = null;
+        boolean duplicate = false;
+
+        ArrayList<PermissionApp> permApps = new ArrayList<>();
+        
+        List<AppOpsManager.PackageOps> pkgs 
+            = mAppOps.getPackagesForOps(new int[]{code});
+        
+        if (pkgs != null) {
+            for (int i=0; i<pkgs.size(); i++) {
+                AppOpsManager.PackageOps pkgOps = pkgs.get(i);
+            
+                try {
+                    appInfo = mPm.getPackageInfo(pkgOps.getPackageName(), PackageManager.GET_PERMISSIONS);
+                } catch (NameNotFoundException e) {
+                    continue;
+                }
+                
+                duplicate = false;
+                for (PermissionApp permDup : permApps) {
+                    if (permDup.getPackageName().equals(appInfo.packageName)) {
+                        duplicate = true;
+                        break;
+                    }
+                }
+
+                if (appInfo.requestedPermissions != null && !duplicate) {
+                    AppPermissionGroup group = AppPermissionGroup.create(mContext,
+                            appInfo,
+                            mGroupName);
+
+                    String label = mSkipUi ? appInfo.packageName
+                            : appInfo.applicationInfo.loadLabel(mPm).toString();
+
+                    Drawable icon = null;
+                    if (!mSkipUi) {
+                        UserHandle userHandle = new UserHandle(
+                                UserHandle.getUserId(appInfo.applicationInfo.uid));
+
+                        icon = mPm.getUserBadgedIcon(
+                                mPm.loadUnbadgedItemIcon(appInfo.applicationInfo, appInfo.applicationInfo),
+                                userHandle);
+                    }
+
+                    PermissionApp permApp = new PermissionApp(appInfo.packageName,
+                                group, label, icon,
+                                appInfo.applicationInfo);
+                    permApps.add(permApp);
+                }
+            }
+        }
+        
+        Collections.sort(permApps);
+        
+        return permApps;
+    }
 
     private void createMap(List<PermissionApp> result) {
         mAppLookup = new ArrayMap<>();
@@ -262,6 +331,13 @@ public class PermissionApps {
     }
 
     private void loadGroupInfo() {
+        /* Unsupported groups */
+        if (Utils.SU_GROUP.equals(mGroupName)) {
+            mLabel = mContext.getString(R.string.exception_group_superuser);
+            return;
+        }
+        
+        /* Normal groups */
         PackageItemInfo info;
         try {
             info = mPm.getPermissionGroupInfo(mGroupName, 0);
